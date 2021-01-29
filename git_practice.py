1de961903e10b773f2cb8ed73ddc7c3d5d8d7696
Nicholas Grosfeld <37131845+NicholasGrosfeld@users.noreply.github.com>
2021-01-29 11:55:31 +1100

    Add files via upload
    A sample script of code.

---
+++ git_practice.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 11:51:41 2021
+
+@author: nick
+"""
+
+def testfunc():
+    print('This is a script of test code.')
+    return
+
+def main():
+    testfunc()
+    return
+
+main()
